63f6ec5c1c88f04a8bf0458f0d13c2725d7691fd
raihanhn <raihanhn20@gmail.com>
2023-01-17 21:26:51 +0600

    6th commit

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import img1 from '../img/cam.png'
+import img2 from '../img/add.png'
+import img3 from '../img/more.png'
+import Messages from './Messages'
+import Input from './Input'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>jane</span>
+        <div className="chatIcons">
+          <img src={img1} alt="" />
+          <img src={img2} alt="" />
+          <img src={img3} alt="" />
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import img from "../img/img.png"
+import attach from "../img/attach.png"
 
-const input = () => {
+const Input = () => {
   return (
-    <div>input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src={attach} alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+        <label htmlFor="file">
+          <img src={img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
-export default input
+export default Input
